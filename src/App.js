a65a7ae74fab4623e01c6972a601890822c51672
Shea Luikart <cluikart@trinity.edu>
2019-08-16 14:22:42 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Menu from "./components/menu";
+
+function App() {
+  return (
+    <div className="App">
+      <Menu/>
+      {/* <div className="menu-highlight">Hellooooo</div> */}
+    </div>
+  );
+}
+
+export default App;
